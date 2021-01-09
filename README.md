4a6b30afdc272cc1eeea8f92ab066560099e90de
Nhon Nguyen <nhnguyen@axon.com>
2021-01-09 13:03:42 +0700

    Add sample and update readme

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cjmcu-kzq-sample
-This is sample of using CJMCU-KZQ board
+This is sample of using CJMCU-KZQ board to control NeoPixel LEDs. I found joy with this cheap board, hope this sample helps you a little bit<br>
+All that we need is the USB to TTL UART CP2102, Arduino IDE, and NeoPixel codes ^_^ <br>
+#### USB CP2102 adapter
+Find download and install the driver for the CP2102 at https://www.silabs.com/developers/usb-to-uart-bridge-vcp-drivers
+
+#### Arduino Settings
+
+On the Arduino IDE set following:
+<ol>
+<li> Board: Arduino Pro or Pro Mini </li>
+<li> Processor: ATmega328P (5V, 16MHz) </li>
+<li> Port: /dev/cu.SLAB_USBtoUART (on MacOS, maybe different on your OS) </li>
+<li> Programmer: USBasp </li>
+</ol>
+
+#### Connection
+For connections between CP2101, CJMCU-KZQ and the NeoPixel LEDs, you can refer to mine below:
+![alt text](images/CJMCU-KZQ.jpg "Title")
+
+You can refer to https://github.com/dieter-l-git/CJMCU-KZQ-Documentation/blob/master/schematic.pdf for more information<br>
+Many thanks to [@dieter-l-git](https://github.com/dieter-l-git/CJMCU-KZQ-Documentation) who did a great work on the schematic of CJMCU-KZQ
+
+
+#### Demo
+https://www.youtube.com/watch?v=ZqvJpk6eiAc
+
+Thank you!
---
+++ tweeking4all-fastled-sample.ino
@@ -0,0 +1,83 @@
+#include <FastLED.h>
+
+#include "FastLED.h"
+// Number of RGB LEDs in the strand
+#define NUM_LEDS 64 
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+// Arduino pin used for Data
+// You can use PIN 6 to 9 as PIN D6-D9 on the CJMCU-KZQ board
+#define PIN 6 
+
+void setup()
+{
+  FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS);
+  FastLED.setBrightness(10);
+}
+
+void loop() { 
+  // one at a time
+  for(int j = 0; j < 3; j++) { 
+    for(int i = 0 ; i < NUM_LEDS; i++ ) {
+      memset(leds, 0, NUM_LEDS * 3);
+      switch(j) { 
+        case 0: leds[i].r = 255; break;
+        case 1: leds[i].g = 255; break;
+        case 2: leds[i].b = 255; break;
+      }
+      FastLED.show();
+      delay(10);
+    }
+  }
+
+  // growing/receeding bars
+  for(int j = 0; j < 3; j++) { 
+    memset(leds, 0, NUM_LEDS * 3);
+    for(int i = 0 ; i < NUM_LEDS; i++ ) {
+      switch(j) { 
+        case 0: leds[i].r = 255; break;
+        case 1: leds[i].g = 255; break;
+        case 2: leds[i].b = 255; break;
+      }
+      FastLED.show();
+      delay(10);
+    }
+    for(int i = NUM_LEDS-1 ; i >= 0; i-- ) {
+      switch(j) { 
+        case 0: leds[i].r = 0; break;
+        case 1: leds[i].g = 0; break;
+        case 2: leds[i].b = 0; break;
+      }
+      FastSPI_LED.show();
+      delay(1);
+    }
+  }
+
+  // Fade in/fade out
+  for(int j = 0; j < 3; j++ ) { 
+    memset(leds, 0, NUM_LEDS * 3);
+    for(int k = 0; k < 256; k++) { 
+      for(int i = 0; i < NUM_LEDS; i++ ) {
+        switch(j) { 
+          case 0: leds[i].r = k; break;
+          case 1: leds[i].g = k; break;
+          case 2: leds[i].b = k; break;
+        }
+      }
+      FastLED.show();
+      delay(3);
+    }
+    for(int k = 255; k >= 0; k--) { 
+      for(int i = 0; i < NUM_LEDS; i++ ) {
+        switch(j) { 
+          case 0: leds[i].r = k; break;
+          case 1: leds[i].g = k; break;
+          case 2: leds[i].b = k; break;
+        }
+      }
+      FastLED.show();
+      delay(3);
+    }
+  }
+}
